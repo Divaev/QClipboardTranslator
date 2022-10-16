c739d4c83d441b9ed255734111606ac8a7273464
Ruslan <Ruslan@mx-laptop>
2022-10-16 12:12:30 +0300

    Some learning examples of the pugilib

--- src/clipboardchkr.cpp
+++ src/clipboardchkr.cpp
@@ -4,32 +4,72 @@
 #include "pugixml.hpp"
 
 int ClipboardChecker::testXML() const {
-    pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_file("/home/ingenigger/Projects/QClipboardTranslator/dict.xdxf");
-    if (!result) {
+
+    std::cout << "---------------Test note output: ----------------------------" << std::endl;
+    pugi::xml_document test_note;
+    //pugi::xml_parse_result result = doc.load_file("/home/ingenigger/Projects/QClipboardTranslator/dict.xdxf");
+    pugi::xml_parse_result test_result = test_note.load_file("note.xml");
+    if (!test_result) {
         std::cout << "Can not open .xml" << std::endl;
         return -1;
     }
     else {
         std::cout << "It's OK" << std::endl;
-        std::cout << "Info: " << result.description() << std::endl;
+        std::cout << "Info: " << test_result.description() << std::endl;
+    }
+    pugi::xml_node my_node_1 = test_note.child("note");                 //pugi::xml_node::child(const char* name) method finds the node by its tag
+    if(my_node_1 != nullptr) {
+        std::cout << "my_node_1 is detected!" << std::endl;
+        std::cout << "name of my_node_1: " << my_node_1.name() << std::endl;
+        std::cout << (my_node_1.value() ? "my_node_1 has a value": "my_node_1 doesn't have a value") << std::endl;
+        std::cout << "value of my_node_1: " << my_node_1.value() << std::endl;
+        std::cout << (strcmp(my_node_1.value(), "/n") ? "my_node_1 value is empty!": "my_node_1 is NOT empty") << std::endl;  //node element(tag) always has a name but never has a value
+        std::cout << "child_value: " << my_node_1.child_value() << std::endl;           //The data of the node is the child content of the node too!
+
+        for(pugi::xml_node curr_child = test_note.child("note"); curr_child; curr_child = curr_child.next_sibling()) {      //The first node with the "note" tag was found
+            std::cout << "curr_child value: " << curr_child.child_value() << std::endl;                                     //Then we started to iterate through all its neighbours
+        }
+
+        pugi::xml_node the_main_parent_node = test_note.child("note");
+        auto curr_node = the_main_parent_node.first_child();
+
+        std::cout << "Going throug all \"note\" childs: " << std::endl;
+        while(curr_node != nullptr) {
+            if(curr_node.type() == 3)               //Depending on the node type we type either node name or node value
+                std::cout << curr_node.value();
+            else if(curr_node.type() == 2)
+                std::cout << curr_node.name() << std::endl;
+
+            curr_node = curr_node.next_sibling();
+        }
     }
 
-    for (pugi::xml_node tool: doc.child("Profile").child("Tools").children("Tool")) {
+    else
+        std::cout << "no my_node_1" << std::endl;
+
+
+
+
+
+    for (pugi::xml_node tool: test_note.child("Profile").child("Tools").children("Tool")) {                           //child(const char *name) returns the first node with needed name
         int timeout = tool.attribute("Timeout").as_int();
 
         if (timeout > 0)
             std::cout << "Tool " << tool.attribute("Filename").value() << " has timeout " << timeout << "\n";
      }
 
-    for (pugi::xml_node word: doc.child("Hello")) {
+    for (pugi::xml_node word: test_note.child("Hello")) {
         std::cout << "word is found" << std::endl;
     }
-    for (pugi::xml_node word: doc.child("to")) {
+    for (pugi::xml_node word: test_note.child("to")) {
         std::cout << "word is found" << std::endl;
     }
 
 
+    std::cout << "---------------Dictionary output: ----------------------------" << std::endl;
+    pugi::xml_document doc;
+    //pugi::xml_parse_result result = doc.load_file("/home/ingenigger/Projects/QClipboardTranslator/dict.xdxf");
+    pugi::xml_parse_result result = doc.load_file("dict.xdxf");         //Loading the .xdxf dictionary from the text file
 
     pugi::xml_node word = doc.child("xdxf");
     pugi::xml_node found_node = word.find_node([](const pugi::xml_node &curr_node){if (!strcmp(curr_node.value(), "final reminder"))
@@ -75,5 +115,5 @@ int ClipboardChecker::testXML() const {
 
     std::cout << "End" << std::endl;
 
-
+    return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,22 @@
 
 #include <clipboardchkr.h>
 
+#include <cstring>
+#include <iostream>
+
 int main(int argc, char *argv[])
 {
+    //const char *old_style_string = new char[10];
+
+    /*
+    char *const old_style_string = new char[10]; //This is constant pointer
+    const char *old_style_string_1 = "Hello world!";
+    std::strcpy(old_style_string, "hello");
+    std::cout << old_style_string << std::endl;
+    std::cout << old_style_string_1 << std::endl;
+    */
+
+
     ClipboardChecker clipChkr;
     clipChkr.testXML();
     QApplication a(argc, argv);
